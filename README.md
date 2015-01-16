9a568192d47a0f1a298273305494068a1aaf7a1c
Jacob Rosenthal <jakerosenthal@gmail.com>
2015-01-15 23:22:21 -0700

    first

---
+++ README.md
@@ -0,0 +1,52 @@
+#react-wizard
+Wizard is a typical wizard element for installs, mulitpart forms, etc. It takes a steps prop with an array of Step objects, start prop with optional integer to skip into the step array, and any othr props you will need later on in Step methods as a data prop. 
+
+#install
+```
+npm install react-wizard
+```
+#steps
+Steps consist of:
+* name - name of step shown to user as list element
+* component - react CreatClass function of your content. 
+* onSubmit - your 'validate' function. If it returns an error the step is not advanced and that error will be shown to user. Successful callback advances to next step.
+
+##example step
+```js
+//ChromeApp.jsx
+var React = require('react');
+
+module.exports = {
+  name: 'Chrome App',
+  onSubmit: function (done){
+    chrome.webstore.install(this.props.data.url, done, function(err){
+      done(new Error('Installation Failed :( ' + err));
+    });
+  },
+  component: React.createClass({
+    render: function(){
+      return <h4>First things first, We need the chrome app to talk to your usb devices. Click Go, and lets Go!</h4>
+    }
+  })
+};
+```
+
+#use
+```js
+var Dash = require('react-wizard');
+var Steps = [
+  require('./Steps/ChromeApp.jsx'),
+  require('./Steps/UnPack.jsx'),
+  require('./Steps/PlugIn.jsx'),
+  require('./Steps/Program.jsx'),
+  require('./Steps/Configure.jsx'),
+  require('./Steps/Done.jsx')
+];
+var url = 'https://chrome.google.com/webstore/detail/gjnfhdmcgnaiogffpdoiecllabiabdee';
+React.render(React.createElement(Dash, {url: url}), document.getElementById('container'));
+```
+##note:
+Back button is automatically disabled on first step.
+The built in buttons are removed from the last step so remember to provide buttons/links on your last step for your user to escape.
+
+Big thanks to @brianshaler for the code review that turned into an entire rewrite
---
+++ src/Wizard.jsx
@@ -0,0 +1,148 @@
+'use strict';
+
+var React = require('react');
+var Button = require('react-bootstrap').Button;
+var ButtonToolbar = require('react-bootstrap').ButtonToolbar;
+var ListGroupItem = require('react-bootstrap').ListGroupItem;
+var ListGroup = require('react-bootstrap').ListGroup;
+var Alert = require('react-bootstrap').Alert;
+
+var Step = React.createClass({
+  getInitialState: function(){
+    return {
+      advanceDisabled: false,
+      retreatDisabled: false,
+    };
+  },
+  onAdvance: function(err){
+    if(err){
+      this.setState({
+        advanceDisabled: false,
+        retreatDisabled: false,
+        currentError: err
+      });
+    }else{
+      this.props.onAdvance();
+    }
+  },
+  onSubmit: function(){
+    this.setState({
+      advanceDisabled: true,
+      retreatDisabled: true
+    });
+    this.props.onSubmit.apply(this, [this.onAdvance]);
+  },
+  render: function(){
+
+    var AdvanceButton;
+    if(!this.props.lastStep){
+      AdvanceButton = React.createElement(Button, {
+        onClick: this.onSubmit,
+        disabled: this.state.advanceDisabled,
+        bsStyle:'primary'
+      },'Onward');
+    }
+
+    var RetreatButton;
+    if(!this.props.lastStep){
+      RetreatButton = React.createElement(Button, {
+        onClick: this.props.onRetreat,
+        disabled: this.props.firstStep || this.state.retreatDisabled,
+      },'back');
+    }
+
+    var ComponentClass = this.props.component;
+
+    var Error;
+    if(this.state.currentError){
+      Error = <Alert bsStyle="danger">
+                <p>{this.state.currentError.message}</p>
+              </Alert>;
+    }
+
+    return <div className="step">
+              <ComponentClass data={this.props.data}/>
+              <ButtonToolbar>
+                {RetreatButton}
+                {AdvanceButton}
+              </ButtonToolbar>
+              {Error}
+           </div>;
+  }
+});
+
+var Wizard = React.createClass({
+  getInitialState: function(){
+    return {
+      currentStep: this.props.start || 0,
+      currentError: null,
+      data: this.props,
+      advanceDisabled: false,
+      retreatDisabled: false
+    };
+  },
+  getDefaultProps: function(){
+    return {
+      steps: []
+    };
+  },
+  advance: function(e){
+    var self = this;
+    self.setState({
+      advanceDisabled: true,
+      retreatDisabled: true
+    });
+  },
+  retreat: function(e){
+    var self = this;
+    self.setState({
+      currentStep: self.state.currentStep-1,
+    });
+  },
+  navigate: function(e){
+    console.log(e);
+  },
+  render: function() {
+
+    var self = this;
+
+    var items = this.props.steps.map(function(step, idx){
+      return (<ListGroupItem key={'nav' + idx} onClick={self.navigate} active={idx === self.state.currentStep ? 'active' : ''}>{step.name}</ListGroupItem>);
+    });
+
+    var step = this.props.steps[this.state.currentStep];
+    var Component = React.createElement(Step, {
+      key: 'step' + this.state.currentStep,
+      onSubmit: step.onSubmit,
+      onAdvance: function(){
+        self.setState({
+          currentError: null,
+          currentStep: self.state.currentStep+1
+        });
+      }, 
+      onRetreat: function(){
+        self.setState({
+          currentError: null,
+          currentStep: self.state.currentStep-1
+        });
+      },
+      component: step.component,
+      data: this.state.data,
+      firstStep: this.state.currentStep === 0,
+      lastStep: this.state.currentStep === this.props.steps.length-1
+    });
+
+    return <div className="wizard">
+            <div className="wizard-nav col-xs-2">
+              <ListGroup>
+                {items}
+              </ListGroup>
+            </div>
+            <div className="wizard-content col-xs-4">
+              {Component}
+            </div>
+           </div>;
+  }
+});
+
+module.exports = Wizard;
