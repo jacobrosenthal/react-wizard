32263a94efd442df1f0de05dcf2e134c3298bf4d
Jacob Rosenthal <jakerosenthal@gmail.com>
2015-01-17 17:31:30 -0700

    add skip example

---
+++ examples/skip/Steps/three.js
@@ -0,0 +1,14 @@
+'use strict';
+var React = require('react');
+
+module.exports = {
+  name: 'Three',
+  onSubmit: function (done){
+    done();
+  },
+  component: React.createClass({displayName: 'component',
+    render: function(){
+      return React.createElement('h4', null, 'Three');
+    }
+  })
+};
